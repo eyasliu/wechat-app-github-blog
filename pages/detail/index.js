83fd9b6f37caf8b87c9e2995e5db8dda35057d99
Eyas <liuyuesongde@163.com>
2016-09-25 14:47:24 +0800

    open source

---
+++ pages/detail/index.js
@@ -0,0 +1,43 @@
+var app = getApp();
+var config = app.globalData.config;
+
+Page({
+	data: {
+		id: 0,
+		item: null
+	},
+	fetchDetail(id){
+		return new Promise(function(resolve, reject){
+			wx.request({
+				url: config.apiUrl + '/issues/' + id,
+				success: function(res){
+					resolve(res.data);
+				},
+				fail: function(err){
+					reject(err);
+				}
+			})
+		}) 
+	},
+	onReady: function(){
+		wx.setNavigationBarTitle({
+			title: this.data.item.title
+		})
+
+	},
+	onLoad: function(){
+		var data = app.globalData.detailData;
+		var that = this;
+		if(data && data.id){
+			this.setData(data)
+			if(!data.item){
+				fetchDetail(data.id)
+				.then(function(data){
+					that.setData({
+						item: data
+					})
+				})
+			}
+		}
+	}
+})
