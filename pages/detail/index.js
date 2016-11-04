f0bf822a6fe00de8e28ac74756e8bbcb1af29a4c
liuyuesong <liuyuesong@kingsoft.com>
2016-11-04 17:41:23 +0800

    详情页解析html

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,10 +1,11 @@
+var WxParse = require('../../wxParse/wxParse.js');
 var app = getApp();
 var config = app.globalData.config;
-
 Page({
 	data: {
 		id: 0,
-		item: null
+		item: null,
+		wxParseData: ''
 	},
 	fetchDetail(id){
 		return new Promise(function(resolve, reject){
@@ -28,13 +29,17 @@ Page({
 	onLoad: function(){
 		var data = app.globalData.detailData;
 		var that = this;
+		data.wxParseData = WxParse('html', data.item.body)
 		if(data && data.id){
 			this.setData(data)
 			if(!data.item){
 				fetchDetail(data.id)
 				.then(function(data){
+					// wxParseData = WxParse('html', data.body)
+					// console.log(wxParseData, data)
 					that.setData({
-						item: data
+						item: data,
+						wxParseData
 					})
 				})
 			}
